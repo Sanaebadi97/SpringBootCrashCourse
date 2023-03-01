e5c05b695a9d82c808a0dc7cd00293640ace8a66
sanaebadi <sana@journeyhero.net>
2023-03-01 15:57:46 +0200

    init commit

---
+++ src/main/java/info/sanaebadi/PhotozClon/PhotozClonApplication.java
@@ -0,0 +1,13 @@
+package info.sanaebadi.PhotozClon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotozClonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotozClonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/sanaebadi/PhotozClon/PhotozClonApplicationTests.java
@@ -0,0 +1,13 @@
+package info.sanaebadi.PhotozClon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozClonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
